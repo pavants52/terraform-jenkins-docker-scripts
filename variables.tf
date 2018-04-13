6ed2dd4e851bae8cd8302490945ad3cfc57846f5
manee2k6 <manee2k6@gmail.com>
2018-04-13 09:39:52 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "aws_region" {
+  description = "The AWS region to create things in."
+  default     = "us-east-2"
+}
+
+# ubuntu-trusty-14.04 (x64)
+variable "aws_ami" {
+  default = {
+    "us-east-2" = "ami-916f59f4"
+    
+  }
+}
+
+variable "key_name" {
+  secKey= "padmavathy"
+  description = "Name of the SSH keypair to use in AWS."
+}
