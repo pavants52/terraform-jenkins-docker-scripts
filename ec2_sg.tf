56891b6e181dffa99704eb46895b118dc801a82b
manee2k6 <manee2k6@gmail.com>
2018-04-25 11:08:15 +0530

    Create ec2_sg.tf

---
+++ ec2_sg.tf
@@ -0,0 +1,56 @@
+resource "aws_security_group" "jenkins-docker"{
+        name        = "jenkins-docker"
+        description = "Used in the terraform"
+
+  # SSH access from anywhere
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Jenkins access from anywhere
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Inbound port for the Jenkins instance created"
+  }
+
+  # Jenkins slave access from anywhere
+  ingress {
+    from_port   = 50000
+    to_port     = 50000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Inbound port for the Jenkins slave instance created"
+  }
+
+  # web access from anywhere
+  ingress {
+    from_port   = 4000
+    to_port     = 4000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Inbound port for the website"
+  }
+  # web access from anywhere
+  ingress {
+    from_port   = 4040
+    to_port     = 4040
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Inbound port for the website"
+  }
+
+
+  # outbound internet access
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
